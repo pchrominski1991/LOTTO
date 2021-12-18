29230f996d3d83261d1008a1c3d688e278bc2f20
piotrchrominski1991 <chrom.piotr@gmail.com>
2021-12-18 16:42:51 +0100

    Lotto - final version

--- LOTTO_simulator.py
+++ LOTTO_simulator.py
@@ -5,26 +5,26 @@ def user_numbs():
     while True:
         try:
             result = []
-            while len(result) < 6:
+            while len(result) < 6:                                     #  Get 6 number from user.
                  choice = int(input("Give your choice:"))
-                 if choice not in result and choice in range(1, 50):
-                    result.append(choice)
+                 if choice not in result and choice in range(1, 50):   # numbers (1-49)
+                    result.append(choice)                              # save numbers as elements of list
                  else:
                      print("NOT correct number")
             break
-        except ValueError:
+        except ValueError:                                             # if user type not a number, try again
              print("It's not a number! Enter the numbers again")
-    print(f" Your numbers: {sorted(result)}")
+    print(f" Your numbers: {sorted(result)}")                          # print sorted list of numbers
     return result
 
 
-def lotto():
+def lotto():                                                           # chose 6 random numbers (1, 49)
     numbers = list(range(1, 50))
     random.shuffle(numbers)
     rnd_num = numbers[0: 6]
     chosen = user_numbs()
     shots = 0
-    for number in chosen:
+    for number in chosen:                                              # check if some random numbers and numbers given by user are the same and count it
         if number in rnd_num:
             shots += 1
     print(f" Lotto numbers: {sorted(rnd_num)}")
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# LOTTO
+# LOTTO simulator
+
+Simple console application - lotto simulator.
+
+- The user selects 6 numbers from the range 1-49.
+- the computer draws 6 numbers
+- we check how many numbers match
+- we secure against entering incorrect data
